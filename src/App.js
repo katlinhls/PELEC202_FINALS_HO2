769160506a51815fd4e5b36f5bf93515277e7e15
katlinhls <kathleenibarrola225@gmail.com>
2026-05-16 07:52:56 +0800

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const API_KEY = "2b3700c30cdd25c386a96dcf811be3e0";
+
+  const fetchWeather = async () => {
+    if (!city) return;
+
+    setLoading(true);
+    setError("");
+    setWeather(null);
+
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+
+      if (!response.ok) {
+        throw new Error("City not found or Invalid city");
+      }
+
+      await new Promise((resolve) => setTimeout(resolve,2000))
+
+      const data = await response.json();
+
+      setWeather({
+        name: data.name,
+        temp: data.main.temp,
+        condition: data.weather[0].description,
+      });
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px", color: "blue" }}>
+      <h1 style={{color: "blue"}}>Weather App ni Katlen na Color Blue</h1>
+
+      <input
+        type="text"
+        placeholder="Enter city..."
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+
+      <button onClick={fetchWeather} style={{ marginLeft: "10px" }}>
+        Search Weather
+      </button>
+
+      {loading && <p>LOADING NA MAG WAIT KA PLS...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      {weather && !loading && (
+        <div style={{ marginTop: "20px" }}>
+          <h2>{weather.name}</h2>
+          <p>Temperature: {weather.temp}°C</p>
+          <p>Condition: {weather.condition}</p>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
